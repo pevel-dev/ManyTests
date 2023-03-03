78c9c38408d404bb57db0d77848eebc1b301e692
pevel-dev <kkollop123@gmail.com>
2023-03-03 15:26:27 +0500

    init

---
+++ ManyTests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace ManyTests;
+
+[TestFixture]
+public class Tests
+{
+
+
+    [Test]
+    public void IsLazy()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [Test]
+    public void DoubleEnumerable()
+    {
+        throw new NotImplementedException();
+    }
+}
