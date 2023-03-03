fca5031e708a0523621b2d463bcbb1105be281d6
Ferdea <sapegin04@mail.ru>
2023-03-03 15:40:50 +0500

    Хорошее название коммита

--- ManyTests/Tests.cs
+++ ManyTests/Tests.cs
@@ -1,18 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using NUnit.Framework;
 
 namespace ManyTests;
 
 [TestFixture]
 public class Tests
 {
-
-
-    [Test]
-    public void IsLazy()
-    {
-        throw new NotImplementedException();
-    }
     
+
+    [Test, Timeout(2000)]
+    public void IsLazy<T>(Func<IEnumerable<T>, IEnumerable<T>> func)
+    {
+        var list = new T[100000];
+        var savedItem = default(T);
+
+        for (var i = 0; i < 100000; ++i)
+        {
+            var item = func(list).FirstOrDefault();
+            savedItem = item;
+        }
+    }
+
     [Test]
     public void DoubleEnumerable()
     {
